f6ebd68686240f954df393fa647d7318a0040e80
Marcos Witzl <57869508+markendos@users.noreply.github.com>
2021-04-08 11:38:06 +0200

    Update net-config.sh

--- scripts/net-config.sh
+++ scripts/net-config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination 192.168.0.4:8443
+sudo iptables -I DOCKER 1 -i eth0 -p tcp --dport 3306 -j DROP
+sudo iptables -I DOCKER 1 -i eth0 -p tcp --dport 27017 -j DROP
