e9cca7a744fbaed7237d945840c5516db69e6701
markendos <markendos@gmail.com>
2021-04-13 00:09:26 +0200

    fixes and improvements

--- src/main/java/es/upo/witzl/proyectotfg/spring/SetupDataLoader.java
+++ src/main/java/es/upo/witzl/proyectotfg/spring/SetupDataLoader.java
@@ -1,5 +1,7 @@
 package es.upo.witzl.proyectotfg.spring;
 
+import es.upo.witzl.proyectotfg.samples.model.Sensor;
+import es.upo.witzl.proyectotfg.samples.repository.SensorRepository;
 import es.upo.witzl.proyectotfg.users.model.Privilege;
 import es.upo.witzl.proyectotfg.users.model.Role;
 import es.upo.witzl.proyectotfg.users.model.User;
@@ -13,10 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
@@ -36,6 +35,9 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private SensorRepository sensorRepository;
+
     // API
 
     @Override
@@ -45,19 +47,24 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
             return;
         }
 
-        // == create initial privileges
+        // create initial privileges
         final Privilege readPrivilege = createPrivilegeIfNotFound("READ_PRIVILEGE");
         final Privilege writePrivilege = createPrivilegeIfNotFound("WRITE_PRIVILEGE");
         final Privilege passwordPrivilege = createPrivilegeIfNotFound("CHANGE_PASSWORD_PRIVILEGE");
 
-        // == create initial roles
+        // create initial roles
         final List<Privilege> adminPrivileges = new ArrayList<>(Arrays.asList(readPrivilege, writePrivilege, passwordPrivilege));
         final List<Privilege> userPrivileges = new ArrayList<>(Arrays.asList(readPrivilege, passwordPrivilege));
         final Role adminRole = createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
-        createRoleIfNotFound("ROLE_USER", userPrivileges);
+        final Role userRole = createRoleIfNotFound("ROLE_USER", userPrivileges);
 
-        // == create initial user
-        createUserIfNotFound("test@test.com", "Admin", "admin", adminRole);
+        // create initial users
+        createUserIfNotFound("admin@test.com", "Admin", "admin", adminRole);
+        createUserIfNotFound("user1@test.com", "User1", "u$er1", userRole);
+        createUserIfNotFound("user2@test.com", "User2", "u$er2", userRole);
+
+        // create initial sensors
+        createSensorIfNotFound("Default", "RAW", "Generic sensor");
 
         alreadySetup = true;
     }
@@ -96,5 +103,21 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
         user.setRole(role);
         user = userRepository.save(user);
         return user;
+    }
+
+    @Transactional
+    Sensor createSensorIfNotFound(final String name, final String alias, final String description) {
+        Optional<Sensor> sensorOptional = sensorRepository.findByAlias(alias);
+        Sensor sensor;
+        if (!sensorOptional.isPresent()) {
+            sensor = new Sensor();
+            sensor.setName(name);
+            sensor.setAlias(alias);
+            sensor.setDescription(description);
+        } else {
+            sensor = sensorOptional.get();
+        }
+        sensor = sensorRepository.save(sensor);
+        return sensor;
     }
 }
